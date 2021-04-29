f47a164bc222602ee8ff3e374b0852c28b33b206
lkaldyne <l.kaldyne@gmail.com>
2021-04-29 05:12:59 -0400

    Finalized landing page

--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,6 @@ particlesJS.load('particles-div', 'particlesjs-config.json');
 gsap.from('#logosvg', { delay: 0.2, y: -100, opacity: 0, duration: 0.5, ease: 'Power4.out' });
 gsap.from('.logo-uline', { delay: 0.7, width: 0, duration: 0.5, ease: 'Power4.out', stagger: 0.1 });
 
-gsap.from('.nav', { delay: 1.5, opacity: 0, duration: 1 })
-gsap.from('#particles-div', { delay: 1.5, opacity: 0, duration: 1 })
-gsap.to('.intrologocontainer', { delay: 1.5, opacity: 0, duration: 1 })
+// gsap.from('.nav', { delay: 1.5, opacity: 0, duration: 1 })
+// gsap.from('#particles-div', { delay: 1.5, opacity: 0, duration: 1 })
+gsap.to('.intrologocontainer', { delay: 1.5, opacity: 0, zIndex: 0, duration: 1 })
